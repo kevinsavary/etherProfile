00419cbb1df7e87abf29279421fa045c7a10217e
kevinsavary <kevin.savary@gmail.com>
2022-05-21 13:49:33 +0100

    initial commit

--- contracts/chainMail.sol
+++ contracts/chainMail.sol
@@ -1,17 +1,22 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract chainMail {
-    string message;
+import "https://https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
-    constructor() {
-        message = "Hello, Hollywood!";
+
+contract chainMail is ERC721Full {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721Full("GameItem", "ITM") public {
     }
 
-    function setMessage(string memory _message) public {
-        message = _message;
-    }
+    function awardItem(address player, string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
 
-    function getMessage() public view returns (string memory) {
-        return message;
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
     }
 }
