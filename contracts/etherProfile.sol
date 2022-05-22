eae35aefd263924d5918471a778b38daae52b2a6
kevinsavary <kevin.savary@gmail.com>
2022-05-22 11:37:27 +0100

    front end and contract wired up

--- contracts/etherProfile.sol
+++ contracts/etherProfile.sol
@@ -1,10 +1,48 @@
 pragma solidity >=0.4.22 <0.9.0;
 
-contract etherProfile {
-    string message;
+import "./Ownable.sol";
+
+contract etherProfile is Ownable {
+    string message; //this is for testing
+
+    // Custom Types
+    struct Profile {
+        uint id;
+        address payable profileOwner;
+        string emailAdress;
+        string profilePhotoUrl;
+    }
+
+    // State variables
+    mapping(uint => Profile) public profiles;
+    uint profileCounter;
+    address payable profileOwner;
+    string emailAdress;
+    string profilePhotoUrl;
+
+    function kill() public onlyOwner {
+        selfdestruct(owner);
+    }
+
+    function addProfile(string memory _emailAdress, string memory _profilePhotoUrl) public {
+        profileCounter++;
+        // need to check if profile owner is already in the contract!
+        profiles[profileCounter] = Profile(
+            profileCounter,
+            payable(msg.sender),
+            _emailAdress,
+            _profilePhotoUrl
+        );
+        message = _profilePhotoUrl; //for testing only - TO REMOVE!
+
+    }
+
+    //add function: is profile address owner already listed?
+
+    // for testing initially
 
     constructor() {
-        message = "Hello, Hollywood!";
+        message = "hello";
     }
 
     function setMessage(string memory _message) public {
@@ -14,4 +52,6 @@ contract etherProfile {
     function getMessage() public view returns (string memory) {
         return message;
     }
+    
+
 }
